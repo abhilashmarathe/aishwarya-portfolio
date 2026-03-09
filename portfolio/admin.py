7981c2e6defbab4e7237e2c6484e7d523d4c14d5
Abhilash Marathe <47948755+abhilashmarathe@users.noreply.github.com>
2026-03-09 12:44:07 +0530

    Update admin.py

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-from .models import Category, Project, ProjectImage
+from .models import Category, Project, ProjectImage, Catalogue
+
 
 admin.site.register(Category)
-admin.site.register(Project)
 admin.site.register(ProjectImage)
+
+
+class CatalogueInline(admin.TabularInline):
+    model = Catalogue
+    extra = 1
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [CatalogueInline]
+
+
+admin.site.register(Project, ProjectAdmin)
